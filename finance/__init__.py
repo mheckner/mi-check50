5c36bc93a018c7b317b519e1090f649243afe3c8
Stefan Butz <stefan260699@gmail.com>
2021-03-26 11:26:01 +0100

    finance: helper class for express app
    Seperate the code to start the express app and to perform requests
    from the checks.

--- finance/__init__.py
+++ finance/__init__.py
@@ -1,36 +1,48 @@
-import check50
 import requests
 import subprocess
-import threading
 import time
 
+import check50
+
 @check50.check()
-def exists():
+def app_exists():
     """app.js exists"""
     check50.exists("app.js")
 
-@check50.check(exists)
-def npm():
+@check50.check(app_exists)
+def npm_install():
     """install node modules"""
     check50.exists("package.json")
     check50.exists("package-lock.json")
     check50.run("npm install").exit(code=0, timeout=10)
     check50.exists("node_modules")
 
-@check50.check(npm)
+@check50.check(npm_install)
 def endpoint_index():
     """Route / works"""
-    proc = subprocess.Popen(['node', 'app.js'])
-
-    # Wait for server to start
-    # XXX: dirty hack
-    time.sleep(1)
-
-    try:
-        r = requests.get('http://localhost:3000')
+    with App() as app:
+        r = app.get('/')
         if r.status_code != 200:
             raise check50.Failure(f'Request failed with Code {r.status_code}')
-    except requests.exceptions.ConnectionError:
-        raise check50.Failure('There is no server running on port 3000')
-    finally:
-        proc.kill()
+
+class App(object):
+    def __init__(self):
+        self.cmd = ['node', 'app.js']
+
+    def __enter__(self):
+        self.proc = subprocess.Popen(self.cmd)
+
+        ## Wait for server to start
+        ## XXX: dirty hack
+        time.sleep(1)
+
+        return self
+
+    def get(self, route):
+        try:
+            return requests.get(f'http://localhost:3000{route}')
+        except requests.exceptions.ConnectionError:
+            raise check50.Failure('There is no server running on port 3000')
+
+    def __exit__(self, type, value, traceback):
+        self.proc.kill()
