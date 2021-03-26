c97b91e754b6bfc71f5adb856014ac46a2a43c3a
Stefan Butz <stefan260699@gmail.com>
2021-03-26 16:51:40 +0100

    finance: use unix sockets
    We need to start the express app before the checks run.
    If we run the check in parallel we do not know which check is
    run first and which is run last. Check50 does not provide hooks
    for global setup or shutdown handler.
    
    The easiest solution is to an instance of the node app for each check.
    To prevent port collision we use unique unix sockets.

--- finance/__init__.py
+++ finance/__init__.py
@@ -1,6 +1,7 @@
+import os
 import requests
+import requests_unixsocket
 import subprocess
-import time
 
 import check50
 
@@ -18,31 +19,34 @@ def npm_install():
     check50.exists("node_modules")
 
 @check50.check(npm_install)
-def endpoint_index():
+def route():
     """Route / works"""
     with App() as app:
         r = app.get('/')
         if r.status_code != 200:
             raise check50.Failure(f'Request failed with Code {r.status_code}')
 
+
 class App(object):
     def __init__(self):
-        self.cmd = ['node', 'app.js']
+        self.session = requests_unixsocket.Session()
 
     def __enter__(self):
-        self.proc = subprocess.Popen(self.cmd)
+        cmd = ['node', 'app.js']
+        env = {'PORT': 'app.sock'}
+        self.proc = subprocess.Popen(cmd, env=env, stdout=subprocess.PIPE)
 
         ## Wait for server to start
         ## XXX: dirty hack
-        time.sleep(1)
+        line = self.proc.stdout.readline()
 
         return self
 
     def get(self, route):
         try:
-            return requests.get(f'http://localhost:3000{route}')
+            return self.session.get(f'http+unix://app.sock{route}')
         except requests.exceptions.ConnectionError:
-            raise check50.Failure('There is no server running on port 3000')
+            raise check50.Failure('There is no server running')
 
     def __exit__(self, type, value, traceback):
         self.proc.kill()
